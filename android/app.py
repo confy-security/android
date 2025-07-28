2392c04a8396d34cd233458769eb418b84ae2b45
Henrique Sebastião <contato@henriquesebastiao.com>
2025-07-28 19:09:57 -0400

    ✏️ refactor: Ajustar estilo de código e adicionar configurações para linting e formatação

--- android/app.py
+++ android/app.py
@@ -1,7 +1,8 @@
-from kivymd.app import MDApp
 from kivy.lang import Builder
+from kivymd.app import MDApp
+
 
 class ConfyApp(MDApp):
     def build(self):
-        self.theme_cls.theme_style = "Dark"
-        return Builder.load_file("android/screens.kv")
+        self.theme_cls.theme_style = 'Dark'
+        return Builder.load_file('android/screens.kv')
--- hot_reload.py
+++ hot_reload.py
@@ -1,16 +1,15 @@
-from kivymd.tools.hotreload.app import MDApp
 from kivy.lang import Builder
-from kivy.utils import hex_colormap
+from kivymd.tools.hotreload.app import MDApp
 
 
 class HotReload(MDApp):
     KV_FILES = ['android/screens.kv']
     DEBUG = True
-    
+
     def build_app(self):
-        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.theme_style = 'Dark'
         # self.theme_cls.primary_palette = "Blue"
         return Builder.load_file('android/screens.kv')
-    
+
 
 HotReload().run()
