5576d9f5ecc202bd3ca9ae30016cee2dd2d5cb3b
Henrique Sebastião <contato@henriquesebastiao.com>
2025-10-30 20:25:20 -0400

    Adicionando primeiro protótipo da tela de login do app
    Testando primeira lógica que obtém username e server_address

--- app/src/main/java/com/henriquesebastiao/confy/MainActivity.kt
+++ app/src/main/java/com/henriquesebastiao/confy/MainActivity.kt
@@ -5,16 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.henriquesebastiao.confy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Actions performed when the connect button is clicked.
+        binding.buttonConnect.setOnClickListener {
+            val username: String = binding.usernameInput.text.toString()
+            val serverAddress: String = binding.serverAddressInput.text.toString()
+
+            binding.usernameView.text = username
+            binding.serverAddressView.text = serverAddress
         }
     }
 }
